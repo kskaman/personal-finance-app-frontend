a310f81f14825912c86dfb3859abc88b3022b164
Kamanpreet Singh <kskaman@gmail.com>
2025-02-05 19:33:53 -0330

    added page nav to transactions page

--- src/pages/TransactionsPage.tsx
+++ src/pages/TransactionsPage.tsx
@@ -2,8 +2,22 @@ import { Stack, Typography } from "@mui/material";
 import SetTitle from "../components/SetTitle";
 import theme from "../theme/theme";
 import PageDiv from "../utilityComponents/PageDiv";
+import SubContainer from "../utilityComponents/SubContainer";
+import Filter from "../components/trasactionsComponents/Filter";
+import TransactionsTable from "../components/trasactionsComponents/TransactionsTable";
+import PageNav from "../components/trasactionsComponents/PageNav";
+import { useState } from "react";
 
 const TransactionsPage = () => {
+  const [pageNum, setPageNum] = useState<number>(() => 1);
+
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const handlePageChange = (newPageNum: number) => {
+    if (newPageNum >= 1 && newPageNum <= numbers[numbers.length - 1]) {
+      setPageNum(newPageNum);
+    }
+  };
   return (
     <>
       <SetTitle title="Transactions" />
@@ -18,6 +32,15 @@ const TransactionsPage = () => {
           >
             Transactions
           </Typography>
+          <SubContainer>
+            <Filter />
+            <TransactionsTable />
+            <PageNav
+              numbers={numbers}
+              selectedPage={pageNum}
+              handlePageSelect={handlePageChange}
+            />
+          </SubContainer>
         </Stack>
       </PageDiv>
     </>
---
+++ src/utilityComponents/Button.tsx
@@ -0,0 +1,59 @@
+interface Props {
+  height?: string;
+  width?: string;
+  padding?: string;
+  color: string;
+  backgroundColor?: string;
+  children: React.ReactNode;
+  borderColor?: string;
+  onClick: () => void;
+  hoverColor: string;
+  hoverBgColor: string;
+}
+
+const Button = ({
+  height = "40px",
+  padding = "16px",
+  backgroundColor = "inherit",
+  color,
+  width = "fit-content",
+  children,
+  borderColor,
+  hoverColor,
+  hoverBgColor,
+  onClick,
+}: Props) => {
+  return (
+    <button
+      style={{
+        height,
+        padding,
+        backgroundColor,
+        width,
+        display: "flex",
+        flexDirection: "row",
+        gap: "16px",
+        alignItems: "center",
+        borderRadius: "8px",
+        border: `1px solid ${borderColor || backgroundColor}`,
+        color,
+        cursor: "pointer",
+      }}
+      onClick={onClick}
+      onMouseEnter={(e) => {
+        e.currentTarget.style.backgroundColor = hoverBgColor || "inherit";
+        e.currentTarget.style.color = hoverColor;
+        e.currentTarget.style.borderColor = hoverColor;
+      }}
+      onMouseLeave={(e) => {
+        e.currentTarget.style.backgroundColor = backgroundColor;
+        e.currentTarget.style.color = color;
+        e.currentTarget.style.borderColor = borderColor || backgroundColor;
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
