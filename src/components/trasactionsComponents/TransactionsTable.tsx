da50df5fee4a688fde9e1016c8c4bfdc5478dbcb
Kamanpreet Singh <kskaman@gmail.com>
2025-02-06 15:31:27 -0330

    completed styling for transactions table

--- src/components/trasactionsComponents/TransactionsTable.tsx
+++ src/components/trasactionsComponents/TransactionsTable.tsx
@@ -1,4 +1,6 @@
 import {
+  Avatar,
+  Box,
   Table,
   TableBody,
   TableCell,
@@ -6,6 +8,13 @@ import {
   TableRow,
 } from "@mui/material";
 import { Transaction } from "../../types/Data";
+import theme from "../../theme/theme";
+import {
+  formatDate,
+  formatNumber,
+  getInitials,
+  getRandomColor,
+} from "../../utils/utilityFunctions";
 
 interface Props {
   txns: Transaction[];
@@ -14,24 +23,124 @@ interface Props {
 const TransactionsTable = ({ txns }: Props) => {
   return (
     <Table>
-      <TableHead sx={{ display: { xs: "none", sm: "block" } }}>
+      <TableHead sx={{ display: { xs: "none", sm: "table-header-group" } }}>
         <TableRow>
-          <TableCell>Recipient/Sender</TableCell>
-          <TableCell>Category</TableCell>
-          <TableCell>Transaction Date</TableCell>
-          <TableCell>Amount</TableCell>
+          <TableCell
+            sx={{
+              fontSize: "12px",
+              color: theme.palette.primary.main,
+              textAlign: "left",
+            }}
+          >
+            Recipient/Sender
+          </TableCell>
+          <TableCell
+            sx={{
+              fontSize: "12px",
+              color: theme.palette.primary.main,
+              textAlign: "left",
+            }}
+          >
+            Category
+          </TableCell>
+          <TableCell
+            sx={{
+              fontSize: "12px",
+              color: theme.palette.primary.main,
+              textAlign: "left",
+            }}
+          >
+            Transaction Date
+          </TableCell>
+          <TableCell
+            sx={{
+              fontSize: "12px",
+              color: theme.palette.primary.main,
+              textAlign: "right",
+            }}
+          >
+            Amount
+          </TableCell>
         </TableRow>
       </TableHead>
 
       <TableBody>
-        {txns.map((txn) => (
-          <TableRow key={txn.date}>
-            <TableCell>{txn.name}</TableCell>
-            <TableCell>{txn.category}</TableCell>
-            <TableCell>{txn.date}</TableCell>
-            <TableCell>{txn.amount}</TableCell>
-          </TableRow>
-        ))}
+        {txns.map((txn, index) => {
+          console.log(index === txns.length - 1);
+          return (
+            <TableRow
+              key={txn.date}
+              sx={{
+                "&:last-child td": { border: 0 },
+              }}
+            >
+              <TableCell
+                sx={{
+                  height: "100%",
+                  textAlign: "left",
+                  color: theme.palette.primary.main,
+                  fontWeight: "bold",
+                  fontSize: "14px",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Avatar
+                    sx={{
+                      fontSize: "12px",
+                      fontWeight: "bold",
+                      color: theme.palette.primary.contrastText,
+                      backgroundColor: getRandomColor(),
+                      width: "40px",
+                      height: "40px",
+                      marginRight: "16px",
+                    }}
+                  >
+                    {getInitials(txn.name)}
+                  </Avatar>
+                  {txn.name}
+                </Box>
+              </TableCell>
+              <TableCell
+                sx={{
+                  textAlign: "left",
+                  color: theme.palette.primary.light,
+                  fontSize: "12px",
+                }}
+              >
+                {txn.category}
+              </TableCell>
+              <TableCell
+                sx={{
+                  textAlign: "left",
+                  color: theme.palette.primary.light,
+                  fontSize: "12px",
+                }}
+              >
+                {formatDate(txn.date)}
+              </TableCell>
+              <TableCell
+                sx={{
+                  textAlign: "right",
+                  color:
+                    txn.amount < 0
+                      ? theme.palette.primary.main
+                      : theme.palette.others.green,
+                  fontSize: "14px",
+                  fontWeight: "bold",
+                }}
+              >
+                {txn.amount < 0 ? "-" : "+"}$
+                {formatNumber(Math.abs(txn.amount))}
+              </TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </Table>
   );
--- src/utils/utilityFunctions.ts
+++ src/utils/utilityFunctions.ts
@@ -57,3 +57,16 @@ export const getRandomColor = () => {
   ];
   return colors[Math.floor(Math.random() * colors.length)];
 };
+
+
+
+export const formatDate = (isoDate: string): string => {
+  const date = new Date(isoDate);
+  
+  // Convert to local time
+  const day = date.getDate();
+  const month = date.toLocaleString("en-US", { month: "short" });
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+};
